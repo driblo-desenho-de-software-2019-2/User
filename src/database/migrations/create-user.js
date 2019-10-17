df25e854d6ae8e9353a4dea79d15d22167660b58
byronkamal <byronkamal@hotmail.com>
2019-10-17 00:18:55 -0300

    Mudando arquivos do server e adicionando tabela do usuario
    Co-authored-by: jpmota2208 <jpmota.unb@gmail.com>
    Co-authored-by: Marcelo Araujo <santos46ma@gmail.com>
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

---
+++ src/database/migrations/create-user.js
@@ -0,0 +1,92 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('user', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+
+      birth_date: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+
+      main_role: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+
+      kick: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+
+      speed: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+
+      pass: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+
+      dribble: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+
+      defense: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+
+      goals: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+
+      victories: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+
+      assistances: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('user');
+  },
+};
--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
-const express = require("express");
+import app from './app';
 
-// Constants
-const PORT = 8001;
-const HOST = "0.0.0.0";
+app.listen(8002);
 
-// App
-const app = express();
-app.get("/", (req, res) => {
-  res.send("User\n");
-});
 
-app.listen(PORT, HOST);
+// const express = require("express");
+
+// // Constants
+// const PORT = 8001;
+// const HOST = "0.0.0.0";
+
+// // App
+// const app = express();
+// app.get("/", (req, res) => {
+//   res.send("User\n");
+// });
+
+// app.listen(PORT, HOST);
