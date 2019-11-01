9750652cb0c28a0df1552289707fb366439c7ee7
Mota <jpmota.unb@gmail.com>
2019-11-01 09:10:02 -0300

    Adicionando funcao de autenticacao e update do usuario
    Co-authored-by: LuisCL94 <lclaudio.tl@gmail.com>

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,19 +12,52 @@ class UserController {
       dribble, defense, goals, victories, assistances
     } = await User.create(req.body);
 
-    return res.json({ 
-      id, 
+    return res.json({
+      id,
       name,
       email,
-      birth_date, 
+      birth_date,
       main_role,
-      kick, 
-      speed, 
-      dribble, 
-      defense, 
-      goals, 
-      victories, 
+      kick,
+      speed,
+      dribble,
+      defense,
+      goals,
+      victories,
       assistances,
+    });
+  }
+
+  async update(req, rest) {
+    //console.log(req.userId);
+    const { email, oldPassword} = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if(email != user.email){
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(400).json({ error: "user already exists." });
+      }
+    }
+
+    if(oldPassword && !(await user.checkPassword(oldPassword))){
+      return res.status(401).json({ error: "Password does not match" });
+    }
+
+    const {id, name, email, birth_date, main_role, kick, speed, dribble, defense} = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      birth_date,
+      main_role,
+      kick,
+      speed,
+      dribble,
+      defense
     });
   }
 }
