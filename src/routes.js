03e8eb1fece655f1a053555739ead7d9f49c211f
matheus-rn <matheus.1nascimento58@gmail.com>
2019-11-16 20:07:30 -0300

    Adicionando método para a autenticação do usuário

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from "./config/multer";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
+import authUser from "./app/utils/util";
 
 import authMiddleware from "./app/middlewares/auth";
 
@@ -17,6 +18,8 @@ routes.post("/sessions", SessionController.store);
 routes.get("/users/all", UserController.listAll);
 routes.get("/users/:id", UserController.searchByid);
 
+routes.post("/auth", authUser);
+
 routes.use(authMiddleware);
 
 routes.put("/users", UserController.update);
@@ -24,5 +27,6 @@ routes.put("/users", UserController.update);
 routes.delete("/users/:id", UserController.deleteById);
 
 routes.post("/files", upload.single("file"), FileController.store);
+
 
 export default routes;
